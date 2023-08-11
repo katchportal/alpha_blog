c5f79f0fdbd40d3e3d93f4ff0be1c902976404a0
Kathy Reyna Portal <katchportal.in@gmail.com>
2023-08-11 14:46:24 +0800

    Add new user signup form, Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] #to not get a route for users#new since its already stated above
 end
