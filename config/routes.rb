4070fb16183d535efac1f585ac7d40d8d96bab7d
Kathy Reyna Portal <katchportal.in@gmail.com>
2023-08-23 10:56:55 +0800

    Add all changes for Associations and Authentication Systems, Many-to-Many Associations and Automated Testing(Integration, Functional, Unit)

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new] #to not get a route for users#new since its already stated above
+  get 'login', to: 'sessions#new' #
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
